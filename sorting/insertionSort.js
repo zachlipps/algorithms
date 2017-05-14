00da32518bc7ff490cadee414841365626ec96c4
Zach Lipps <zacharylipps10@gmail.com>
2017-05-14 12:29:21 -0700

    add insertionSort in place

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,3 +1,5 @@
+// not in place
+
 function insertionSort(array) {
   const result = [];
 
@@ -21,3 +23,21 @@ function insertionSort(array) {
 
 
 console.log(insertionSort([4, 32, 1, 44, 8]));
+
+
+// in place
+
+
+const insertionSortIP = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] < array[j]) {
+        array.splice(j, 0, array.splice(i, 1)[0]);
+      }
+    }
+  }
+  return array;
+};
+
+
+console.log(insertionSortIP([4, 32, 1, 44, 8, 99, 38, 38, -10]));
