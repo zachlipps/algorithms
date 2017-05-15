2159b5982421e51f01509e88178d1243f86405ed
Zach Lipps <zacharylipps10@gmail.com>
2017-05-14 18:47:35 -0700

    updated merge function

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,20 +1,17 @@
-function mergeSorted(arr1, arr2) {
-  const sorted = [];
-  let currentIdx1 = 0;
-  let currentIdx2 = 0;
+function merge(left, right) {
+  let sorted = [];
+  let iLeft = 0;
+  let iRight = 0;
 
-  while (currentIdx1 !== arr1.length && currentIdx2 !== arr2.length) {
-    const el1 = arr1[currentIdx1];
-    const el2 = arr2[currentIdx2];
-    if (el1 && el1 < el2 || !el2) {
-      sorted.push(el1);
-      currentIdx1++;
-    }
-    sorted.push(el2);
-    currentIdx2++;
+  while (sorted.length < (left.length + right.length)) {
+    if (iLeft === left.length) sorted = sorted.concat(right.slice(iRight));
+    else if (iRight === right.length) sorted = sorted.concat(left.slice(iLeft));
+    else if (left[iLeft] <= right[iRight]) sorted.push(left[iLeft++]);
+    else sorted.push(right[iRight++]);
   }
+
   return sorted;
 }
 
 
-console.log(mergeSorted([1, 2, 3], [2, 3, 4]));
+console.log(merge([1, 2, 3], [2, 3, 4]));
